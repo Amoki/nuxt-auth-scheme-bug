1e17c1ef6831a0ef7cab1b33693b0e0b3750e278
amoki <hugo@bimdata.io>
2020-09-07 14:41:40 +0200

    initial

---
+++ auth/oidc_scheme.js
@@ -0,0 +1,3 @@
+import Oauth2Scheme from '@nuxtjs/auth-next/dist/schemes/oauth2'
+
+export default class OidcScheme extends Oauth2Scheme {}
---
+++ nuxt.config.js
@@ -0,0 +1,104 @@
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {},
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  auth: {
+    strategies: {
+      keycloak: {
+        // scheme: 'oauth2',
+        scheme: '~/auth/oidc_scheme',
+        endpoints: {
+          authorization: `https://nope/auth/realms/bimdata/protocol/openid-connect/auth`,
+          userInfo: `https://nope/auth/realms/bimdata/protocol/openid-connect/userinfo`,
+          token: `https://nope/auth/realms/bimdata/protocol/openid-connect/token`,
+          logout: `https://nope/auth/realms/bimdata/protocol/openid-connect/logout`
+        },
+        token: {
+          property: 'access_token',
+          type: 'Bearer',
+          name: 'Authorization',
+          maxAge: 1800
+        },
+        refreshToken: {
+          property: 'refresh_token',
+          maxAge: 60 * 60 * 24 * 30
+        },
+        responseType: 'code',
+        grantType: 'authorization_code',
+        clientId: 'nope',
+        scope: ['profile', 'email'],
+        codeChallengeMethod: 'S256',
+      },
+    },
+  },
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
+
+  router: {
+    middleware: ['auth']
+  },
+}
